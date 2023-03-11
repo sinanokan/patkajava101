34da051cd838f52ae44be2460cc2842128338cd9
sinan okan <okanyurtsever14@gmail.com>
2023-03-12 00:38:21 +0300

    Yinelemeli Üs alma commit

---
+++ YinelemeliUsAlma/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    static  int usAlma(int taban,int us){
+        if(us<1)return 1;
+        else return taban * usAlma(taban,us-1);
+    }
+    public static void main(String[] args) {
+        Scanner tara = new Scanner(System.in);
+        int t,u;
+
+        System.out.print("Sayının tabanını giriniz: ");
+        t = tara.nextInt();
+
+        System.out.print("Sayının üssünü giriniz: ");
+        u = tara.nextInt();
+
+        System.out.println("Sonuc: " + usAlma(t,u));
+
+
+    }
+}
